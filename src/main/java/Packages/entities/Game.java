aef235d7d8fb765fc265458521dd50c07b9ce6e4
rocketpowers <rocketpowerswillian@gmail.com>
2023-05-14 19:53:06 -0300

    geme seed works

--- src/main/java/Packages/entities/Game.java
+++ src/main/java/Packages/entities/Game.java
@@ -23,26 +23,32 @@ public class Game {
 	private Integer year;
 	
 	private String genre;
-	private String platform;
+	private String platforms;
+	private String score;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
-	private String longDescripton;
+	
+	@Column(columnDefinition = "TEXT")
+	private String longDescription;
 
 	public Game() {
 
 	}
 
-	public Game(Long id, String title, Integer year, String genre, String platform, String imgUrl,
-			String shortDescription, String longDescripton) {
+	public Game(Long id, String title, Integer year, String genre, String platforms, String score, String imgUrl,
+			String shortDescription, String longDescription) {
 
 		this.id = id;
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
-		this.platform = platform;
+		this.platforms = platforms;
+		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
-		this.longDescripton = longDescripton;
+		this.longDescription = longDescription;
 	}
 
 }
