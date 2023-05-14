35db456df8dc30a1da0cd6d3bc3d877c0e904df2
rocketpowers <rocketpowerswillian@gmail.com>
2023-05-14 19:07:48 -0300

    project create

---
+++ src/main/java/Packages/DsListApplication.java
@@ -0,0 +1,13 @@
+package Packages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsListApplication.class, args);
+	}
+
+}
